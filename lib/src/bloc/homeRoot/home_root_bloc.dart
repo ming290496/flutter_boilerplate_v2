7a54fc891fa147df0d643f1efdc0c0c150f5ed0b
Nicodemus <nicodemusdemuse@gmail.com>
2021-01-03 20:52:53 +0700

    add bottom navigation bar

--- lib/src/bloc/homeRoot/home_root_bloc.dart
+++ lib/src/bloc/homeRoot/home_root_bloc.dart
@@ -1,11 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_skeleton_v2/src/bloc/bloc.dart';
+import 'package:rxdart/rxdart.dart';
 
-class HomeRootBloc implements Bloc{
+enum NavBarEnum { HOME, ARTICLE, ADD, MEDIA, PROFILE }
+
+class HomeRootBloc implements Bloc {
   BuildContext context;
+
+  final _navBar = BehaviorSubject<NavBarEnum>();
+
+  Stream<NavBarEnum> get navBar => _navBar.stream;
+
+  Function(NavBarEnum) get changeNavBar => _navBar.sink.add;
+
+  NavBarEnum get navBarValue => _navBar.value;
+
+  pickItem(index) {
+    switch (index) {
+      case 0:
+        changeNavBar(NavBarEnum.HOME);
+        break;
+      case 1:
+        changeNavBar(NavBarEnum.ARTICLE);
+        break;
+      case 2:
+        changeNavBar(NavBarEnum.ADD);
+        break;
+      case 3:
+        changeNavBar(NavBarEnum.MEDIA);
+        break;
+      case 4:
+        changeNavBar(NavBarEnum.PROFILE);
+        break;
+    }
+  }
+
+  int getItem(NavBarEnum navBarEnum) {
+    switch (navBarEnum) {
+      case NavBarEnum.HOME:
+        return 0;
+        break;
+      case NavBarEnum.ARTICLE:
+        return 1;
+        break;
+      case NavBarEnum.ADD:
+        return 2;
+        break;
+      case NavBarEnum.MEDIA:
+        return 3;
+        break;
+      case NavBarEnum.PROFILE:
+        return 4;
+        break;
+      default:
+        return 0;
+        break;
+    }
+  }
 
   @override
   void dispose() {
-    // TODO: implement dispose
+    _navBar.close();
   }
 }
--- lib/src/screen/homeRoot/home_root_screen.dart
+++ lib/src/screen/homeRoot/home_root_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_skeleton_v2/src/bloc/bloc_provider.dart';
+import 'package:flutter_skeleton_v2/src/bloc/homeRoot/home_root_bloc.dart';
 
 class HomeRootScreen extends StatefulWidget {
   @override
@@ -8,11 +10,116 @@ class HomeRootScreen extends StatefulWidget {
 class _HomeRootScreenState extends State<HomeRootScreen> {
   @override
   Widget build(BuildContext context) {
+    final bloc = BlocProvider.of<HomeRootBloc>(context);
+
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Home Root Screen'),
-        ),
+      bottomNavigationBar: StreamBuilder<NavBarEnum>(
+        stream: bloc.navBar,
+        initialData: NavBarEnum.HOME,
+        builder: (context, snapshot) {
+          return BottomNavigationBar(
+            key: Key('NavBarMenu'),
+            backgroundColor: Colors.white,
+            type: BottomNavigationBarType.fixed,
+            onTap: (value) {
+              bloc.pickItem(value);
+            },
+            currentIndex: bloc.getItem(snapshot.data),
+            elevation: 0,
+            selectedFontSize: 11,
+            unselectedFontSize: 10,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home,
+                  size: kToolbarHeight * 0.35,
+                ),
+                title: Padding(
+                  padding: EdgeInsets.only(top: 2),
+                  child: Text('HOME'),
+                ),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.event_note,
+                  size: kToolbarHeight * 0.35,
+                ),
+                title: Padding(
+                  padding: EdgeInsets.only(top: 2),
+                  child: Text('ARTIKEL'),
+                ),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person_add,
+                  size: kToolbarHeight * 0.35,
+                ),
+                title: Padding(
+                  padding: EdgeInsets.only(top: 2),
+                  child: Text('ADD'),
+                ),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.headset,
+                  size: kToolbarHeight * 0.35,
+                ),
+                title: Padding(
+                  padding: EdgeInsets.only(top: 2),
+                  child: Text('MEDIA'),
+                ),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.people,
+                  size: kToolbarHeight * 0.35,
+                ),
+                title: Padding(
+                  padding: EdgeInsets.only(top: 2),
+                  child: Text('PROFIL'),
+                ),
+              )
+            ],
+          );
+        },
+      ),
+      body: StreamBuilder<NavBarEnum>(
+        stream: bloc.navBar,
+        initialData: NavBarEnum.HOME,
+        builder: (context, snapshot) {
+          switch (snapshot.data) {
+            case NavBarEnum.HOME:
+//                        homeBloc.homeRootArgument = bloc.argument;
+              return Center(
+                child: Text('HOME'),
+              );
+              break;
+            case NavBarEnum.ARTICLE:
+              return Center(
+                child: Text('ARTICLE'),
+              );
+              ;
+              break;
+            case NavBarEnum.ADD:
+              return Center(
+                child: Text('ADD'),
+              );
+              break;
+            case NavBarEnum.MEDIA:
+              return Center(
+                child: Text('MEDIA'),
+              );
+              ;
+              break;
+            case NavBarEnum.PROFILE:
+              return Center(
+                child: Text('PROFILE'),
+              );
+              ;
+              break;
+          }
+          return Container();
+        },
       ),
     );
   }
